1defda159a4f5306edd34f18204e5cbb9ebe4e32
kr3110 <n.nkr3110@gmail.com>
2018-04-15 04:26:44 -0400

    done

--- backend/app.js
+++ backend/app.js
@@ -1,68 +1,110 @@
 var express = require('express');
-var twilio= require('twilio');
+var twilio = require('twilio');
 const language = require('@google-cloud/language');
 var keys = require('./keys.json');
 var bodyParser = require('body-parser')
+var giphy = require('giphy-api')(keys.giphy);
 var app = express();
-
 
 var twilioClient = new twilio(keys.twilioSID, keys.twilioToken);
 var client = new language.LanguageServiceClient();
 
-const negative = 0.5;
-const positive = -0.4;
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
+const positive = 0.3;
+const negative = -0.3;
+
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
 });
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.post('/',function(req,res){
+app.post('/', function(req, res) {
   var conversation = req.body.data;
-  //var phoneNumber = req.body.phoneNumber;
+  var phoneNumber = req.body.phoneNumber;
   var query = {
-    type:"PLAIN_TEXT",
+    type: "PLAIN_TEXT",
     language: "EN",
     content: conversation
   };
   var docSentiment;
   var docEntity;
 
-  client.analyzeSentiment({document: query})
+  client.analyzeSentiment({
+      document: query
+    })
     .then(function(results) {
-      docSentiment= results[0].documentSentiment;
-      client.analyzeEntities({document: query})
+      docSentiment = results[0].documentSentiment;
+      client.analyzeEntities({
+          document: query
+        })
         .then(function(results) {
           var temp = results[0];
-          docEntity= temp.entities[0].name;
+          docEntity = temp.entities[0].name;
         })
-        .then(function(){
+        .then(function() {
+          if(docSentiment.score<= negative){
+            docEntity+=" sad";
+          }
+          console.log(docEntity);
+          console.log(docSentiment);
+          giphy.search({
+            q: docEntity,
+            rating: 'g',
+            limit: 5,
+          }, function(err, response) {
+             var ran= Math.floor(Math.random() * Math.floor(5));
+             var data = response.data[ran];
+             console.log(data);
+             sendText(phoneNumber,data)
+             res.json({
+               success:"true"
+             })
+          });
 
         })
-        .catch(function(err){
-          console.error('ERROR:', err);
+        .catch(function(err) {
+          res.json({
+            success:"false",
+            error: err
+          })
         });
 
     })
-    .catch(function(err){
-      console.error('ERROR:', err);
+    .catch(function(err) {
+      res.json({
+        success:"false",
+        error: err
+      })
     });
 
 
 });
+async function sendText(phoneNumber, data){
+  twilioClient.messages.create({
+      body: data.title,
+      to: phoneNumber,
+      from: '+18035003905',
+      mediaUrl: data.images.downsized.url
+    })
+    .then( function(message){
+        console.log(message);
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+  }
 
 
-
-var port= 8080;
+var port = 8080;
 app.listen(port);
